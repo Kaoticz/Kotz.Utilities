c930465c5a02916a7ecad5e5934c26c3538e039f
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2024-03-15 20:13:30 -0300

    Added Clear method to AsyncEvent

--- Kotz.Events/AsyncEvent.cs
+++ Kotz.Events/AsyncEvent.cs
@@ -7,7 +7,7 @@ namespace Kotz.Events;
 /// <typeparam name="T2">The event arguments.</typeparam>
 public sealed class AsyncEvent<T1, T2> where T2 : EventArgs
 {
-    private readonly List<AsyncEventHandler<T1, T2>> _handlers = new();
+    private readonly List<AsyncEventHandler<T1, T2>> _handlers = [];
 
     /// <summary>
     /// The event handler used for registration of new handlers.
@@ -25,14 +25,20 @@ public sealed class AsyncEvent<T1, T2> where T2 : EventArgs
         => _handlers;
 
     /// <summary>
+    /// Removes all handlers from the invocation list.
+    /// </summary>
+    public void Clear()
+        => _handlers.Clear();
+
+    /// <summary>
     /// Invokes the handlers registered in this event and waits for
     /// all of them to complete execution.
     /// </summary>
     /// <param name="sender">The sender object.</param>
-    /// <param name="args">The event arguments.</param>
-    public Task InvokeAsync(T1 sender, T2 args)
+    /// <param name="eventArgs">The event arguments.</param>
+    public Task InvokeAsync(T1 sender, T2 eventArgs)
     {
-        var tasks = _handlers.Select(x => x.Invoke(sender, args));
+        var tasks = _handlers.Select(x => x.Invoke(sender, eventArgs));
         return Task.WhenAll(tasks);
     }
 }
